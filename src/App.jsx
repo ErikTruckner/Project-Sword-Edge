1f200ebb055561cf48b8b714825db22a8835b610
Erik Truckner <eriktruckner@comcast.net>
2023-03-09 09:21:31 -0500

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,5 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import Box from './Box'
-import ComplexSolarSystem from './ComplexSolarSystem'
-import { Stars } from '@react-three/drei'
-
-export default function App() {
-  console.log(Canvas)
-
-  return (
-    <Canvas>
-      <color args={['#000000']} attach='background' />
-      <Stars />
-      {/* <ambientLight /> */}
-      <pointLight intensity={0.9} position={[0, 0, 0]} />
-      <Box />
-
-      <OrbitControls />
-    </Canvas>
-  )
+const App = () => {
+  return <div>Erik Truckner</div>
 }
+
+export default App
