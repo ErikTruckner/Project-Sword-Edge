5c65d693310b7c69b3f50c635d359aa4047f4de3
Erik Truckner <eriktruckner@comcast.net>
2023-03-09 08:16:25 -0500

    deleted unneeded components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-// import Box from './Box'
-// import Castle from './Castle'
-// import SimpleSolarSystem from './SimpleSolarSystem'
+import Box from './Box'
 import ComplexSolarSystem from './ComplexSolarSystem'
 import { Stars } from '@react-three/drei'
 
@@ -16,12 +14,8 @@ export default function App() {
       <Stars />
       {/* <ambientLight /> */}
       <pointLight intensity={0.9} position={[0, 0, 0]} />
-      <ComplexSolarSystem />
-      {/* <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} /> */}
-      {/* <Box position={[-0.8, 0, 0]} />
-      <Box position={[0.8, 0, 0]} /> */}
+      <Box />
+
       <OrbitControls />
     </Canvas>
   )
