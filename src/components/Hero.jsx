5f19a42ab72310a0454963336cf65ccf713255c2
Erik Truckner <eriktruckner@comcast.net>
2023-03-10 08:44:40 -0500

    gltf visible

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,21 @@ const Hero = () => {
       <div
         className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#9153ff]' />
+          <div className='w-5 h-5 rounded-full bg-[#1180ff]' />
+          <div className='w-1 sm:h-80 h-40 blue-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#3ac0ff]'>Erik</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I'm a Full Stack Developer Web Developer{' '}
+            <br className='sm:block hidden' />
+            Specializing in React, Node, and 3D Web Integrations
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   )
 }
