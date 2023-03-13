24e88d6ff26f22f65af0c691b98dd0bd77256ac4
Erik Truckner <eriktruckner@comcast.net>
2023-03-13 08:05:08 -0400

    tweaking styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,15 +44,19 @@ const About = () => {
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        As a software developer, I possess technical expertise in utilizing
-        advanced technologies like React, Next, Vue, Nuxt, and Three to deliver
-        reliable and impactful solutions. I am committed to creating exceptional
-        user experiences through good design and interface, incorporating
-        emerging technologies like 3D to push the boundaries of frontend visual
-        styles and enhance user engagement. My dedication to continuous
-        improvement and learning enables me to deliver innovative solutions that
-        prioritize quality and reliability. Overall, I strive to deliver
-        software solutions that leave a lasting impact and exceed expectations.
+        As a creative software developer, I possess technical expertise in
+        utilizing frontend JavaScript technologies such as React, Next, Vue,
+        Nuxt, and Three.js to deliver reliable and impactful visual solutions.
+        To store necessary information I utilize Node.js and Express.js combined
+        with the approriate database for the porject, either SQL or noSQL.{' '}
+        <br />
+        I'm committed to creating exceptional user experiences through good
+        design and interface, incorporating emerging technologies like 3D to
+        push the boundaries of frontend visual styles and enhance user
+        engagement. My dedication to continuous improvement and learning enables
+        me to deliver innovative solutions that prioritize quality and
+        reliability. Overall, I strive to deliver software solutions that leave
+        a lasting impact and exceed expectations.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
