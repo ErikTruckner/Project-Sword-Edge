54daafaca2164648d74b430235182452bbfb1c80
Erik Truckner <eriktruckner@comcast.net>
2023-03-13 11:11:31 -0400

    finalized card design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -23,7 +23,7 @@ const ServiceCard = ({ index, title, icon }) => {
             speed: 450,
           }}
           className='glowing-shadow rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'>
-          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <img src={icon} alt={title} className=' w-16 h-16 object-contain' />
           <h3 className='text-white text-[20px] font-bold text-center'>
             {title}{' '}
           </h3>
